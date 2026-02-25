8ad40e64843afdf646afa5fd3705e59b8895d044
aothymoon59 <aothymoon59@gmail.com>
2026-02-26 01:14:59 +0600

    :recycle: make service finder works section reusable

---
+++ src/components/about/HowServiceFinderWork.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+const HowServiceFinderWork = () => {
+    return (
+        <section className="bg-white aon-how-service-area">
+            <div className="container">
+                <div className="section-content">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-12">
+                            <span className="aon-sub-title">Steps</span>
+                            <h2 className="sf-title">How Service Finder Works</h2>
+                        </div>
+
+                        <div className="col-lg-8 col-md-12">
+                            <div className="aon-step-blocks">
+                                <div className="row">
+                                    <div className="col-md-4 col-sm-4 m-b30">
+                                        <div className="aon-step-section step-position-1 aon-icon-effect">
+                                            <div className="aon-step-icon aon-icon-box">
+                                                <span>
+                                                    <i className="aon-icon">
+                                                        <img alt="" src="/images/step-icon/1.png" />
+                                                    </i>
+                                                </span>
+                                            </div>
+                                            <div className="aon-step-info">
+                                                <h4 className="aon-title">Describe Your Task</h4>
+                                                <p>
+                                                    This helps us determine which Taskers are abest
+                                                    job.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-4 col-sm-4 m-b30">
+                                        <div className="aon-step-section step-position-2 aon-icon-effect">
+                                            <div className="aon-step-icon">
+                                                <span>
+                                                    <i className="aon-icon">
+                                                        <img alt="" src="/images/step-icon/2.png" />
+                                                    </i>
+                                                </span>
+                                            </div>
+                                            <div className="aon-step-info">
+                                                <h4 className="aon-title">Choose a Tasker</h4>
+                                                <p>
+                                                    This helps us determine which Taskers are abest
+                                                    job.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-4 col-sm-4 m-b30">
+                                        <div className="aon-step-section step-position-3 aon-icon-effect">
+                                            <div className="aon-step-icon">
+                                                <span>
+                                                    <i className="aon-icon">
+                                                        <img alt="" src="/images/step-icon/3.png" />
+                                                    </i>
+                                                </span>
+                                            </div>
+                                            <div className="aon-step-info">
+                                                <h4 className="aon-title">Live Smarter</h4>
+                                                <p>
+                                                    This helps us determine which Taskers are abest
+                                                    job.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HowServiceFinderWork
