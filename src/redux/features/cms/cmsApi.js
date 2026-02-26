cdfcc58699ff5e6e0e3b793697a126b6cd14445e
aothymoon59 <aothymoon59@gmail.com>
2026-02-27 01:23:16 +0600

    :sparkles: added api's in rtk query

--- src/redux/features/cms/cmsApi.js
+++ src/redux/features/cms/cmsApi.js
@@ -1,5 +1,4 @@
 import { baseApi } from "../../api/baseApi";
-import { tagTypes } from "../../tagTypes";
 
 const cmsApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
@@ -9,185 +8,44 @@ const cmsApi = baseApi.injectEndpoints({
         method: "GET",
       }),
     }),
-
-    getCmsContent: build.query({
-      query: (page) => ({
-        url: `/cms-content`,
-        method: "GET",
-        params: { page },
-      }),
-      providesTags: [tagTypes.SITE_SETTINGS_CONTENT],
-      meta: { skipAuth: true },
-    }),
-
-    // New dynamic CMS endpoints
-    getAllCmsPages: build.query({
+    getSettingsValues: build.query({
       query: () => ({
-        url: `/admin/cms-content/all-pages`,
+        url: `/get-setting-values`,
         method: "GET",
       }),
-      providesTags: [tagTypes.CMS_CONTENT],
     }),
-    getCmsContentByPage: build.query({
-      query: (page) => ({
-        url: `/admin/cms-content/page/${page}`,
-        method: "GET",
-      }),
-      providesTags: [tagTypes.CMS_CONTENT],
-    }),
-    createOrUpdateDynamicContent: build.mutation({
-      query: (data) => ({
-        url: `/admin/cms-content`,
-        method: "POST",
-        body: data,
-      }),
-      invalidatesTags: [tagTypes.CMS_CONTENT, tagTypes.SITE_SETTINGS_CONTENT],
-    }),
-    deleteCmsField: build.mutation({
-      query: ({ page, record_id }) => ({
-        url: `/admin/cms-content/field`,
-        method: "DELETE",
-        body: { page, record_id },
-      }),
-      invalidatesTags: [tagTypes.CMS_CONTENT],
-    }),
-    deleteCmsPage: build.mutation({
-      query: (page) => ({
-        url: `/admin/cms-content/page/${page}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: [tagTypes.CMS_CONTENT],
-    }),
-    getDashboardData: build.query({
+    getAllBlogs: build.query({
       query: () => ({
-        url: `/dashboard`,
+        url: `/blogs`,
         method: "GET",
       }),
     }),
-
-    // dropdown options
-    createStartupCategoriesDD: build.mutation({
-      query: (data) => ({
-        url: `/admin/startup-categories`,
-        method: "POST",
-        body: data,
-      }),
-      invalidatesTags: [tagTypes.DD_STARTUP_CATEGORIES],
-    }),
-    getAllStartupCategoriesDD: build.query({
-      query: () => ({
-        url: `/startup-categories`,
-        method: "GET",
-      }),
-      providesTags: [tagTypes.DD_STARTUP_CATEGORIES],
-    }),
-    updateStartupCategoriesDD: build.mutation({
-      query: ({ payload, id }) => ({
-        url: `/admin/startup-categories/${id}`,
-        method: "PUT",
-        body: payload,
-      }),
-      invalidatesTags: [tagTypes.DD_STARTUP_CATEGORIES],
-    }),
-    deleteStartupCategoriesDD: build.mutation({
+    getSingleBlog: build.query({
       query: (id) => ({
-        url: `/admin/startup-categories/${id}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: [tagTypes.DD_STARTUP_CATEGORIES],
-    }),
-
-    createFundingStageCategoriesDD: build.mutation({
-      query: (data) => ({
-        url: `/admin/founding-stages`,
-        method: "POST",
-        body: data,
-      }),
-      invalidatesTags: [tagTypes.DD_FUNDING_STAGES],
-    }),
-    getAllFundingStageCategoriesDD: build.query({
-      query: () => ({
-        url: `/founding-stages`,
+        url: `blog-single/${id}`,
         method: "GET",
       }),
-      providesTags: [tagTypes.DD_FUNDING_STAGES],
     }),
-    updateFundingStageCategoriesDD: build.mutation({
-      query: ({ payload, id }) => ({
-        url: `/admin/founding-stages/${id}`,
-        method: "PUT",
-        body: payload,
-      }),
-      invalidatesTags: [tagTypes.DD_FUNDING_STAGES],
-    }),
-    deleteFundingStageCategoriesDD: build.mutation({
-      query: (id) => ({
-        url: `/admin/founding-stages/${id}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: [tagTypes.DD_FUNDING_STAGES],
-    }),
-
-    createCurrenciesDD: build.mutation({
-      query: (data) => ({
-        url: `/admin/currencies`,
-        method: "POST",
-        body: data,
-      }),
-      invalidatesTags: [tagTypes.DD_CURRENCIES],
-    }),
-    getAllCurrenciesDD: build.query({
+    getAllCategories: build.query({
       query: () => ({
-        url: `/currencies`,
+        url: `/get-categories`,
         method: "GET",
       }),
-      providesTags: [tagTypes.DD_CURRENCIES],
     }),
-    updateCurrenciesDD: build.mutation({
-      query: ({ payload, id }) => ({
-        url: `/admin/currencies/${id}`,
-        method: "PUT",
-        body: payload,
-      }),
-      invalidatesTags: [tagTypes.DD_CURRENCIES],
-    }),
-    deleteCurrenciesDD: build.mutation({
+    getSingleCategory: build.query({
       query: (id) => ({
-        url: `/admin/currencies/${id}`,
-        method: "DELETE",
+        url: `/single-category/${id}`,
+        method: "GET",
       }),
-      invalidatesTags: [tagTypes.DD_CURRENCIES],
     }),
   }),
 });
 
 export const {
   useGetHomePageContentQuery,
-  // Legacy exports for backward compatibility
-  useCreateOrUpdateCmsContentMutation,
-  useGetCmsContentQuery,
-
-  // New dynamic CMS exports
-  useGetAllCmsPagesQuery,
-  useGetCmsContentByPageQuery,
-  useCreateOrUpdateDynamicContentMutation,
-  useDeleteCmsFieldMutation,
-  useDeleteCmsPageMutation,
-
-  // Dashboard exports
-  useGetDashboardDataQuery,
-
-  // Dropdown options
-  useCreateStartupCategoriesDDMutation,
-  useGetAllStartupCategoriesDDQuery,
-  useUpdateStartupCategoriesDDMutation,
-  useDeleteStartupCategoriesDDMutation,
-  useCreateFundingStageCategoriesDDMutation,
-  useGetAllFundingStageCategoriesDDQuery,
-  useUpdateFundingStageCategoriesDDMutation,
-  useDeleteFundingStageCategoriesDDMutation,
-  useCreateCurrenciesDDMutation,
-  useGetAllCurrenciesDDQuery,
-  useUpdateCurrenciesDDMutation,
-  useDeleteCurrenciesDDMutation,
+  useGetSettingsValuesQuery,
+  useGetAllBlogsQuery,
+  useGetSingleBlogQuery,
+  useGetAllCategoriesQuery,
+  useGetSingleCategoryQuery,
 } = cmsApi;
