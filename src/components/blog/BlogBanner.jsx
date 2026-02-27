808c5d7e5328573ca363ea4a726af7134ca9dd97
mahejabin <mahejabinmou.99@gmail.com>
2026-02-28 01:54:01 +0600

    blog page reusable component create

---
+++ src/components/blog/BlogBanner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../shared/Banner";
+
+const BlogBanner = () => {
+  return (
+    <Banner
+      title="Blog"
+      backgroundImage="/images/banner/job-banner.jpg"
+      breadcrumb="Blog"
+    />
+  );
+};
+
+export default BlogBanner;
