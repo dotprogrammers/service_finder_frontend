8b390c3f86cc77fa7e441794bcd62cf647a343a0
mahejabin <mahejabinmou.99@gmail.com>
2026-02-28 01:59:45 +0600

    pricing page reusable component create

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -8,34 +8,8 @@ import AllBlogs from "../components/blog/AllBlogs";
 const BlogPage = () => {
   return (
     <div className="page-content">
-      {/* Banner */}
-      {/* <div className="aon-page-benner-area">
-        <div
-          className="aon-page-banner-row relative"
-          style={{ backgroundImage: "url(/images/banner/job-banner.jpg)" }}
-        >
-          <div
-            className="sf-overlay-main absolute inset-0"
-            style={{ opacity: "0.8", backgroundColor: "#022279" }}
-          />
-          <div className="sf-banner-heading-wrap relative">
-            <div className="sf-banner-heading-area">
-              <div className="sf-banner-heading-large">Blog</div>
-              <div className="sf-banner-breadcrumbs-nav">
-                <ul className="list-inline">
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>Blog</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <BlogBanner />
       <AllBlogs />
-      {/* Content wrapper */}
     </div>
   );
 };
